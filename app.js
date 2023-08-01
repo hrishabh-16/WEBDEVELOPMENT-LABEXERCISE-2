19220178bcccbb5d8ce84b88f8ad727cf134c7ab
SUVAJITKARMAKAR <gitsuvajit@gmail.com>
2023-08-02 01:47:42 +0530

    PORTFOLIO-LA-2 | header section created and designed using parallax effect | SUVAJITKARMAKAR

---
+++ app.js
@@ -0,0 +1,8 @@
+// AUTO-TYPE-CDN-CUSTOMIZATION 
+let autotype = new Typed(".auto-type", {
+    strings: ["WEB-DEVELOPER","BOOK-WORM", "WEB-DESIGNER", "VIDEO-EDITOR"],
+    typeSpeed: 150,
+    backSpeed: 100,
+    loop: true
+});
+
