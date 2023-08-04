3a292653993fd6ed86e6c5ebae7ea0b65e6d4e54
SUVAJITKARMAKAR <gitsuvajit@gmail.com>
2023-08-04 15:21:30 +0530

    PORTFOLIO-LA-2 | mywork section created , mygallery added and set up | SUVAJITKARMAKAR

--- scripts/app.js
+++ scripts/app.js
@@ -6,35 +6,3 @@ let autotype = new Typed(".auto-type", {
     loop: true
 });
 
-// SCROLL-ANIMATION 
-const scrolloffset = 200;
-const scrollElement = document.querySelector(".js-scroll");
-
-const elementInView = (el, offset = 0) => {
-    const elementTop = el.getBoundingClientRect().top;
-
-    return (
-         elementTop <= ((
-            window.innerHeight || document.documentElement.clientHeight ) - offset )
-        );
-};
-
-const displayScrollElement = () => {
-    scrollElement.classList.add('scrolled');
-}
-
-const hideScrollElement = () => {
-    scrollElement.classList.remove('scrolled');
-}
-
-const handleScrollAnimation = () => {
-    if (elementInView (scrollElement, scrolloffset)) {
-        displayScrollElement();
-    } else {
-        hideScrollElement();
-    }
-}
-
-window.addEventListener('scroll', () => {
-    handleScrollAnimation();
-})
